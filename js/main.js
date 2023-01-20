edccdb9a4f5841f84a36bfd5200d08a1c50363a1
VincenzoTavassi <info@egadivacanze.it>
2023-01-20 15:41:45 +0100

    semicolon

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ let cognomeUtente = prompt('Qual è il tuo cognome?');
 let colorePreferito = prompt('Qual è il tuo colore preferito?');
 
 document.getElementById('welcome').innerHTML = `
-La tua password è ${nomeUtente + cognomeUtente + colorePreferito + Math.floor(Math.random() * 100)} e qui c'è altro testo.
-`
+La tua password è ${nomeUtente + cognomeUtente + colorePreferito + Math.floor(Math.random() * 100)} e qui c'è altro testo.`;
 
 // 'La tua password è ' + nomeUtente + cognomeUtente + colorePreferito + Math.floor(Math.random() * 100);
 
